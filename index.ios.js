c9c20086b65748c6514a2dd928c7e5e4763cd3ee
Stephen Laughton <stephenlaughton@gmail.com>
2017-03-15 13:42:16 -0700

    woooo!

--- index.ios.js
+++ index.ios.js
@@ -17,7 +17,7 @@ export default class testGitFlow extends Component {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native new Feature!
+          Welcome to React Native another! new Feature!
         </Text>
         <Text style={styles.instructions}>
           To get started, edit index.ios.js
